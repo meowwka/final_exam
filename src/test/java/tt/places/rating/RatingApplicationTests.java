d98e28a482cd896f6069cd519061b100d1baed93
Taazim Temirova <tezi.temirova@gmail.com>
2020-08-15 10:08:05 +0600

    initial commit

---
+++ src/test/java/tt/places/rating/RatingApplicationTests.java
@@ -0,0 +1,13 @@
+package tt.places.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
